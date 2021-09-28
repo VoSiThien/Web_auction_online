a7878dc9bc41978b4c3f2fc7ed7368fc7741fc93
nguyen-go2joy <nguyen.le@go2joy.vn>
2021-09-28 20:36:17 +0700

    update authentication

--- routers_config/api.js
+++ routers_config/api.js
@@ -5,7 +5,10 @@ const API = express.Router()
 const favoriteProductController = require('../controllers/favoriteProduct.controller')
 const bidProductController = require('../controllers/bidProduct.controller')
 const sellerController = require('../controllers/seller.controller')
+const authController = require('../controllers/authentication.controller')
 
+
+API.use('/auth', authController)
 API.use('/favorite-product', favoriteProductController)
 API.use('/bid', bidProductController)
 API.use('/seller', sellerController)
--- routers_config/routers.js
+++ routers_config/routers.js
@@ -1,22 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const authentication = require('../middlewares/authentication')
 
 const API = require('./api')
 
-router.use('/api', API)
+router.use('/api', authentication.verifyToken, API)
 
 router.use((req, res, next) => {
-	return res.status(400).json({
-		errorMessage: 'API Url Not Found',
-		statusCode: 1
-	})
+    return res.status(400).json({
+        errorMessage: 'API Url Not Found',
+        statusCode: 1
+    })
 })
 
 router.use((err, req, res, next) => {
-	return res.status(500).json({
-		err,
-		statusCode: 1
-	})
+    return res.status(500).json({
+        err,
+        statusCode: 1
+    })
 })
 
 module.exports = router
