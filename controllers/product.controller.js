d37bf295d6f239a2a63983378fd4420c82f6b30a
NguyenMinhTam-max <nguyenminhtam1106999@gmail.com>
2021-09-29 12:43:02 +0700

    update product detail

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -145,13 +145,13 @@ router.post('/search', prodValidation.productSearching, async (req, res) => {
 	var numberOfProduct = await knex.raw(`SELECT count(pr.prod_id)
 	FROM tbl_product pr join tbl_categories cat on pr.prod_category_id = cat.cate_id
 	WHERE cat.ts @@ to_tsquery('english', '${searchKey}') ${AndOrCondition} pr.ts @@ to_tsquery('english', '${searchKey}')`)
-	
+
 	numberOfProduct = Number(numberOfProduct.rows[0].count)
 	var numberOfPage = 1
 	if (numberOfProduct > limit) {
 		numberOfPage = Math.ceil(numberOfProduct / limit)
 	}
-	
+
 	//full-text-search for category & product name
 	var result = await knex.raw(`with product as (
 		SELECT *
@@ -164,7 +164,7 @@ router.post('/search', prodValidation.productSearching, async (req, res) => {
 	on img.prod_img_product_id = pr.prod_id
 	order by ${filterField} ${orderBy}`)
 	result = result.rows
-	
+
 	var prodList = []
 	if ((result.length != 0)) {
 		var index = 0
@@ -204,12 +204,59 @@ router.post('/search', prodValidation.productSearching, async (req, res) => {
 			index += 1
 		}
 	}
-	
+
 	return res.status(200).json({
 		numberOfPage: numberOfPage,
 		numberProduct: numberOfProduct,
 		listProduct: prodList,
 		statusCode: successCode
+	})
+})
+
+/////PRODUCT DETAIL API
+/*1.Get product detail information*/
+router.get('/details/:id', async (req, res) => {
+	const { id } = req.params
+
+	var date = new Date();
+	var prod = await knex('tbl_product')
+		.where('prod_id', id)
+
+	if (prod.length === 0) {
+		return res.status(400).json({
+			errorMessage: " Product record doesn't exist!",
+			statusCode: 1
+		})
+	}
+	
+	var prodObject = {}
+	var accountList = await accountModel.getActiveUser()
+	const prodResult = await knex.from('tbl_product')
+		.where('prod_id', id)
+		.returning('*')
+		.then(async (rows) => {
+			prodObject = rows[0];
+			
+			//get holder & seller information
+			prodObject.prod_price_holder = accountList.find((priceHolder) => priceHolder.acc_id == prodObject.prod_price_holder).acc_full_name
+			console.log(prodObject.prod_price_holder)
+			prodObject.prod_seller = accountList.find((seller) => seller.acc_id == prodObject.prod_seller_id).acc_full_name
+			
+			//get sub-iamges
+			var imageResult = await knex.from('tbl_product_images')
+				.where('prod_img_product_id', prodObject.prod_id);
+			prodObject['prod_img'] = imageResult.map(attr => attr.prod_img_data);
+		})
+	if (prodObject) {
+		return res.status(200).json({
+			listProductDetail: prodObject,
+			statusCode: successCode
+		})
+	}
+
+	return res.status(200).json({
+		listProductDetail: [],
+		statusCode: errorCode
 	})
 })
 
